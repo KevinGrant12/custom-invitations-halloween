ea854ce8482191e232632ed77078f4fd3ffd8cad
Kevin <grantcreative23@gmail.com>
2019-10-17 00:12:04 -0700

    Updated landing message

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,7 +8,7 @@ export default class Landing extends Component {
       <section className="landing">
         <h3>Customize Your Own</h3>
         <h1>Halloween Party Invitation</h1>
-        <p>Receive a custom party invitation package to send out for your holiday event.</p>
+        <p>Create a custom party invitation to send out for your Halloween party or event.</p>
         <Link to='/editor'><i class="fas fa-skull"></i>Get Started</Link>
       </section>
     );
